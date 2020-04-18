299c1dc97cbee2761c2de6020dfbfc93916d5ca5
Philip Soares <philipsoares1@gmail.com>
2020-04-18 10:36:26 -0300

    Updates

---
+++ precache-manifest.882768960a40b629d37bf5614b9e6faa.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "bbb832b67bdfaee116f48f864a6f1421",
+    "url": "/palpiteiro/index.html"
+  },
+  {
+    "revision": "8ccd4a5cde30718e2d98",
+    "url": "/palpiteiro/static/css/main.28f4a190.chunk.css"
+  },
+  {
+    "revision": "875bcae52e2c14bf6172",
+    "url": "/palpiteiro/static/js/2.06cf4759.chunk.js"
+  },
+  {
+    "revision": "8ccd4a5cde30718e2d98",
+    "url": "/palpiteiro/static/js/main.b68625d0.chunk.js"
+  },
+  {
+    "revision": "32559738f0ffc95b1624",
+    "url": "/palpiteiro/static/js/runtime-main.39f88209.js"
+  }
+]);
